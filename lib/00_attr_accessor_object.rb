88ea733196c1d5ce9497fe3a08a81a2c4a695817
abaez8031 <abaez8031@gmail.com>
2023-02-14 17:12:55 -0500

    second commit

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -2,11 +2,11 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
     names.each do |name|
-      define_method(name) do
-        `@#{name}`
+      define_method(name) do 
+        instance_variable_get("@#{name}")
       end
-      define_method(`#{name}=`) do |newname|
-        `@#{name}=` newname
+      define_method("#{name}=") do |new_name|
+        instance_variable_set("@#{name}",new_name)
       end
     end
 
